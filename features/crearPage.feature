16ae10e5792eee29d7d55e4fe20e60782eb00778
albealhe <aalvarezh@sura.com.co>
2022-11-15 21:47:23 -0500

    steps in separate files

--- features/crearPage.feature
+++ features/crearPage.feature
@@ -1,7 +1,7 @@
 Feature: Crear page
 
 @user1 @web
-Scenario: Como usuario inicio sesion y creo una pagina con titulo y descripcion
+Scenario: Como usuario inicio sesion y creo una pagina con titulo New page test
   Given I navigate to page "http://localhost:2368/ghost/#/signin"
   And I wait for 5 seconds
   When I enter email "<USERNAME>"
@@ -14,12 +14,32 @@ Scenario: Como usuario inicio sesion y creo una pagina con titulo y descripcion
   And I wait for 3 seconds
   When I click on new page
   When I type the tittle 'New page test'
-  And I wait for 2 seconds
-  When I click on publish
+  And I wait for 3 seconds
+  When I go to editor section
   And I wait for 1 seconds
-  When I click on publish continue
-  And I wait for 1 seconds
-  When I click on publish page
   When I go to pages section
+  When I go pages publish
   And I wait for 2 seconds
   Then I check the first row with tittle 'New page test'
+
+
+@user2 @web
+Scenario: Como usuario inicio sesion creo una pagina y la dejo en borrador
+  Given I navigate to page "http://localhost:2368/ghost/#/signin"
+  And I wait for 5 seconds
+  When I enter email "<USERNAME>"
+  And I wait for 2 seconds
+  And I enter password "<PASSWORD>"
+  And I wait for 2 seconds
+  And I click next
+  And I wait for 5 seconds
+  When I go to pages
+  And I wait for 3 seconds
+  When I click on new page
+  When I type the tittle 'New page test in draft'
+  And I wait for 5 seconds
+  When I go to editor section
+  When I go to pages section
+  And I wait for 5 seconds
+  When I go pages drafts
+  Then I check the first row with tittle 'New page test in draft'
---
+++ features/web/step_definitions/page.js
@@ -0,0 +1,69 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const expect = require('chai').expect;
+const assert = require("assert");
+
+When("I go to pages", async function () {
+    let element = await this.driver.$('.gh-nav-top > ul:nth-child(2) > li:nth-child(2)');
+    return await element.click();
+});
+
+When("I click on new page", async function () {
+    let button = await this.driver.$(".view-actions > a");
+    return await button.click();
+});
+  
+When("I type the tittle {string}", async function (tittle) {
+    let tittleElement = await this.driver.$("[placeholder='Page title']");
+    return await tittleElement.setValue(tittle);
+});
+
+When("I click on settings", async function () {
+    let settings = await this.driver.$(".gh-btn:nth-child(2)");
+    return await settings.click(); 
+});
+
+When("I click on publish", async function () {
+    let publishButton = await this.driver.$(".gh-publish-trigger");
+    return await publishButton.click();
+});
+
+When("I click on publish continue", async function () {
+    let continueButton = await this.driver.$(".gh-publish-cta >button");
+    return await continueButton.click();
+});
+
+When("I click on publish page", async function () {
+    let publishPage = await this.driver.$(".gh-publish-cta >button");
+    return await publishPage.click();
+});
+
+  
+When("I go to editor section", async function () {
+    let editorSection = await this.driver.$(".gh-btn-editor");
+    return  await  await editorSection.click();;
+});
+
+When("I go to pages section", async function () {
+    let pagesSection = await this.driver.$(".gh-editor-back-button");
+    return  await pagesSection.click();
+});
+
+When("I go pages publish", async function () {
+    let allPages = await this.driver.$("div.gh-contentfilter-menu.gh-contentfilter-type > div.ember-view.ember-basic-dropdown-trigger.ember-power-select-trigger.gh-contentfilter-menu-trigger");
+    await allPages.click();
+    let pagesPublish = await this.driver.$(".ember-power-select-options > li:nth-child(3)");
+    return  await pagesPublish.click();
+});
+     
+Then("I check the first row with tittle {string}", async function (tittle) {
+    tittleElementText = await this.driver.$(".pages-list > li:nth-child(1) > a:nth-child(1) > h3").getText();
+    return assert.equal(tittleElementText,tittle);
+});
+
+When("I go pages drafts", async function () {
+    let allPages = await this.driver.$("div.gh-contentfilter-menu.gh-contentfilter-type > div.ember-view.ember-basic-dropdown-trigger.ember-power-select-trigger.gh-contentfilter-menu-trigger");
+    await allPages.click();
+    let pagesPublish = await this.driver.$(".ember-power-select-options > li:nth-child(2)");
+    return  await pagesPublish.click();
+});
+  
